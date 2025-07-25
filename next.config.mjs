b449071bbe45a2cadbd218bd156c7312c208ed8f
Syed Ahtisham Ali <111875652+0SyedAli@users.noreply.github.com>
2025-07-25 23:03:19 +0500

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
     return [
       {
         source: '/',
-        destination: '/auth/login',
+        destination: '/auth/signin',
         permanent: false,
       },
     ];
